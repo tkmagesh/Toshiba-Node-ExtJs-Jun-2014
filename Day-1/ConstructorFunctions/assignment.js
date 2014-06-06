f27e5b61d61e90ddbe6dd897bfb1978a84afbe18
tkmagesh <tkmagesh77@gmail.com>
2014-06-06 17:21:29 +0530

    Day - 1 Complete

--- Day-1/ConstructorFunctions/assignment.js
+++ Day-1/ConstructorFunctions/assignment.js
@@ -8,3 +8,23 @@ Modify the above constructor function that exposes accessor methods exhibiting t
 1. "id" should be readonly
 2. "name" cannot be assigned a null or empty string value
 3. "salary" cannot be assigned a value less than the current salary
+
+function Employee(id,name,salary){
+	var _id = id,
+		_name = name,
+		_salary = salary;
+
+	this.id = function(){
+		return _id;
+	};
+	this.name = function(val){
+		if (typeof val === "undefined") return _name;
+		if (val !== "") _name = val;
+	};
+	this.salary = function(val){
+		if (typeof val === "undefined") return _salary;
+		if (val > _salary) _salary = val;
+	}
+}
+
+
