b86ea3f2914eeadd96d0a1881172ca7ad80c5a3e
tkmagesh <tkmagesh77@gmail.com>
2014-06-09 09:42:25 +0530

    data seed

---
+++ Day-3/JavaScript/products.js
@@ -0,0 +1,10 @@
+
+
+var products = [
+	{id : 1, name : "pen", cost : 90 , units : 9 , category : 1},
+	{id : 6, name : "hen", cost : 40 , units : 3 , category : 2},
+	{id : 3, name : "ten", cost : 20 , units : 8 , category : 1},
+	{id : 9, name : "den", cost : 40 , units : 2 , category : 2},
+	{id : 7, name : "zen", cost : 50 , units : 7 , category : 1},
+	{id : 8, name : "ken", cost : 70 , units : 5 , category : 2}
+]
